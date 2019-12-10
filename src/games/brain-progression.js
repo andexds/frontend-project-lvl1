ba4cca9b9db80b264c8c45fffd0b377cb09fdea8
Anashkin Andrey <andexds@gmail.com>
2019-12-10 10:38:39 +0300

    Fix magic number in brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -8,13 +8,14 @@ const showWelcomeMessage = () => {
 };
 
 const gameBrainProgression = () => {
+  const countOfNumbersInProgression = 10;
   const start = getRandomNumber(0, 10);
   const step = getRandomNumber(1, 10);
-  const positionOfEmpty = getRandomNumber(0, 9);
+  const positionOfEmpty = getRandomNumber(0, countOfNumbersInProgression - 1);
   let progression = '';
   const correctAnswer = String(start + step * positionOfEmpty);
 
-  for (let i = 0; i < 10; i += 1) {
+  for (let i = 0; i < countOfNumbersInProgression; i += 1) {
     if (i !== positionOfEmpty) {
       progression += ` ${start + step * i} `;
     } else {
