3fb687830854f93cb99036b56b7243cd7c4d709c
Andrey Anashkin <andexds@Andreys-MacBook-Pro.local>
2020-01-25 15:57:59 +0300

    Make index.js engine file

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,25 +1,21 @@
-import readlineSync from 'readline-sync';
 import getRandomNumber from '../helpers/random';
 import startGame from '..';
 
-const showWelcomeMessage = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-};
 const isEven = (number) => number % 2 === 0;
 
 const gemaBrainEven = () => {
   const currentNumber = getRandomNumber(0, 100);
   console.log(`Question: ${currentNumber}`);
-  const correctAnswer = isEven(currentNumber) ? 'yes' : 'no';
+  const answer = isEven(currentNumber) ? 'yes' : 'no';
 
-  const answer = readlineSync.question('Your answer: ');
-  if (answer === correctAnswer) {
-    console.log('\nCorrect!\n');
-    return true;
-  }
-  console.log(`\n'${answer}' is wrong answer ;(. Correct answer was ${correctAnswer}.\n`);
-  return false;
+  return {
+    question: currentNumber,
+    answer,
+  };
 };
 
-export default () => startGame(showWelcomeMessage, gemaBrainEven);
+export default () => startGame({
+  gameName: 'Brain Even Game',
+  regulations: 'Answer "yes" if the number is even, otherwise answer "no".',
+  round: gemaBrainEven,
+});
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import readlineSync from 'readline-sync';
 
-const startGame = (showWelcomeMessage, game) => {
-  showWelcomeMessage();
+const startGame = (game) => {
+  const { gameName, regulations, round } = game;
+
+  console.log(`Welcome to the ${gameName}!`);
+  console.log(regulations);
+
   const name = readlineSync.question('What is your name?\n');
   console.log(`Hello, ${name}`);
-  let rightAnswer = 0;
+  let countRightAnswer = 0;
 
-  while (rightAnswer < 3) {
-    const isCorrect = game();
-    if (isCorrect) {
-      rightAnswer += 1;
+  while (countRightAnswer < 3) {
+    const roundStep = round();
+    const { question, answer } = roundStep;
+
+    const userAnswer = readlineSync.question(`${question}\n`);
+    if (userAnswer === answer) {
+      countRightAnswer += 1;
     } else {
+      console.log(`\n'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\n`);
       return;
     }
   }
