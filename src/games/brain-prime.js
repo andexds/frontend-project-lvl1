5256773c8bccade0d35e8f8fb0a11fcf153ec4ce
Andrey Anashkin <andexds@Andreys-MacBook-Pro.local>
2020-01-29 00:46:03 +0300

    fix bug in prime function

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,7 +2,9 @@ import getRandomNumber from '../helpers/random';
 import startGame from '..';
 
 const isPrime = (n) => {
-  for (let i = 2; i < n / 2; i += 1) {
+  if (n < 2) return false;
+
+  for (let i = 2; i <= n / 2; i += 1) {
     if (n % i === 0) return false;
   }
   return true;
